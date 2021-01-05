fc10e7e2133c0e6ac85f6dde8914068735127c3d
Yelena <example@gmail.com>
2021-01-04 21:54:35 -0800

    first practice

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberOfFilms=prompt("сколько фильмов вы уже посмотрели","");
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false};
+
+    const a=prompt("один из последних фильмов",""),
+          b=prompt("на сколько оцените его",""),
+          c=prompt("один из последних фильмов",""),
+          d=prompt("на сколько оцените его","");
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
